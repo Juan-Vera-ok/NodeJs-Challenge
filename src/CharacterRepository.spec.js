a4ece03a7981ec0f8da7349401f2c765c5a1e7d5
Juan <juanvera2001@hotmail.com>
2022-01-04 00:30:22 -0300

    Test de characterRepository funcionando

--- src/CharacterRepository.spec.js
+++ src/CharacterRepository.spec.js
@@ -1,18 +1,26 @@
 import CharacterRepository from "./CharacterRepository.js";
 import {strict as assert} from 'assert';
 
-testInsert();
+const repo= new CharacterRepository();
+const name="Ricardo";
+const age= 27;
+const weight = 67;
+const bio = "Hola soy Ricardo";
+const character = repo.insert({name,age,weight,bio});
+const character2 = repo.insert({name,age,weight,bio});
 
+assert.deepEqual(repo.list(),{[character.id]:character,[character2.id]:character2});
 
-function testInsert(){
-    const repo= new CharacterRepository();
-    const name="Ricardo";
-    const age= 27;
-    const weight = 67;
-    const bio = "Hola soy Ricardo";
-    const character = repo.insert({name, age, weight, bio});
+const insertedCharacter = repo.find(character.id);
 
-    const insertedCharacter = repo.find(character.id);
-    
-    assert.deepEqual(insertedCharacter,{name, age, weight,bio,id:character.id});
-}
+assert.deepEqual(insertedCharacter,{name, age, weight,bio,id:character.id});
+
+repo.update(character.id,{name:"Lampone",age:40,bio:"Hola soy Lampone"})
+
+assert.deepEqual
+(repo.find(character.id),{name:"Lampone",age:40,weight,bio:"Hola soy Lampone",id:character.id})
+
+repo.delete(character.id)
+
+assert.equal(repo.find(character.id),undefined)
+
